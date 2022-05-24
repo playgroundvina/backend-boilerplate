56e539be0615eb42c176c57d4c6a6aae51cd54ef
Tan Le <tanle@Tans-MacBook-Pro.local>
2022-05-24 09:06:18 +0700

    Sample module

--- src/modules/offers/entities/offer.entity.ts
+++ src/modules/offers/entities/offer.entity.ts
@@ -1,1 +1,8 @@
-export class Offer {}
+import { AbstractEntity } from "src/common/entities/abstract-entity"
+import { Column, Entity, Index } from "typeorm"
+
+@Entity('offer')
+export class Offer extends AbstractEntity {
+    @Column({ default: 0 })
+    offerId: number
+}
--- src/modules/offers/offers.module.ts
+++ src/modules/offers/offers.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OffersService } from './offers.service';
 import { OffersController } from './offers.controller';
+import { Offer } from './entities/offer.entity';
 
 @Module({
+  TypeOrmModule.forFeature([Offer]),
   controllers: [OffersController],
   providers: [OffersService]
 })
-export class OffersModule {}
+export class OffersModule { }
